0d849e302cb4b21c0847128ccda4cc2b35fa680f
Jeremy Gold <jeremy.gold@ikegps.com>
2015-12-05 23:37:04 +1300

    Updating to include second leg, and initial walk cycle

--- Arduino/hx12k_ServoTest/hx12k_ServoTest.ino
+++ Arduino/hx12k_ServoTest/hx12k_ServoTest.ino
@@ -1,50 +1,68 @@
 #include <Servo.h>
 
-Servo pitch_servo;
-Servo shin_servo;
-Servo hip_servo;
+Servo mid_left_pitch_servo;
+Servo mid_left_shin_servo;
+Servo mid_left_hip_servo;
+Servo front_left_pitch_servo;
+Servo front_left_shin_servo;
+Servo front_left_hip_servo;
 
 int pos = 0;    
 int min_pos = 100;
 int max_pos = 180;
 int front_pos = 30;
 int back_pos = 120;
+int front_hip_offset = 50;
 
 int delay_ms = 5;
 int shin_offset = 00;
+int front_shin_offset = -50;
+int front_pitch_offset = -50;
 
 void setup() {
-  shin_servo.attach(9);
-  pitch_servo.attach(10);
-  hip_servo.attach(11);  
+  mid_left_shin_servo.attach(9);
+  mid_left_pitch_servo.attach(10);
+  mid_left_hip_servo.attach(11);  
+
+  front_left_shin_servo.attach(3);
+  front_left_pitch_servo.attach(5);
+  front_left_hip_servo.attach(6);  
 }
 
 void moveUp() {
   for (pos = min_pos; pos <= max_pos; pos += 1) { 
-    pitch_servo.write(pos);             
-    shin_servo.write(pos+shin_offset);
+    mid_left_pitch_servo.write(pos);             
+    mid_left_shin_servo.write(pos+shin_offset);
+
+    front_left_pitch_servo.write(max_pos - (pos + front_pitch_offset));
+    front_left_shin_servo.write(max_pos - (pos + front_shin_offset));
     delay(delay_ms);                       
   }
 }
 
 void moveDown() {
   for (pos = max_pos; pos >= min_pos; pos -= 1) { 
-    pitch_servo.write(pos);              
-    shin_servo.write(pos+shin_offset);
+    mid_left_pitch_servo.write(pos);              
+    mid_left_shin_servo.write(pos+shin_offset);
+
+    front_left_pitch_servo.write(max_pos - (pos + front_pitch_offset));              
+    front_left_shin_servo.write(max_pos - (pos + front_shin_offset));
     delay(delay_ms);                       
   }
 }
 
 void moveForward() {
   for (pos = front_pos; pos <= back_pos; pos += 1) { 
-    hip_servo.write(pos);             
+    mid_left_hip_servo.write(pos);             
+    front_left_hip_servo.write(back_pos - pos + front_hip_offset);             
     delay(delay_ms);                       
   }
 }
 
 void moveBack() {
   for (pos = back_pos; pos >= front_pos; pos -= 1) { 
-    hip_servo.write(pos);             
+    mid_left_hip_servo.write(pos);             
+    front_left_hip_servo.write(back_pos - pos + front_hip_offset);             
     delay(delay_ms);                       
   }
 }
