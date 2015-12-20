680afd195d28f9cc4ffdd397b4fa91b660eedd0d
Jeremy Gold <jeremy.gold@ikegps.com>
2015-12-20 21:21:32 +1300

    Updating to have better speed control

--- Arduino/hx12k_ServoTest/hx12k_ServoTest.ino
+++ Arduino/hx12k_ServoTest/hx12k_ServoTest.ino
@@ -7,6 +7,8 @@ Servo front_left_pitch_servo;
 Servo front_left_shin_servo;
 Servo front_left_hip_servo;
 
+Servo centered_servo;
+
 int pos = 0;    
 int min_pos = 100;
 int max_pos = 180;
@@ -14,7 +16,8 @@ int front_pos = 30;
 int back_pos = 120;
 int front_hip_offset = 50;
 
-int delay_ms = 5;
+int servo_step = 2;
+int delay_ms = 10;
 int shin_offset = 00;
 int front_shin_offset = -50;
 int front_pitch_offset = -50;
@@ -24,13 +27,15 @@ void setup() {
   mid_left_pitch_servo.attach(10);
   mid_left_hip_servo.attach(11);  
 
-  front_left_shin_servo.attach(3);
+  //front_left_shin_servo.attach(3);
   front_left_pitch_servo.attach(5);
   front_left_hip_servo.attach(6);  
+  
+  centered_servo.attach(3);
 }
 
 void moveUp() {
-  for (pos = min_pos; pos <= max_pos; pos += 1) { 
+  for (pos = min_pos; pos <= max_pos; pos += servo_step) { 
     mid_left_pitch_servo.write(pos);             
     mid_left_shin_servo.write(pos+shin_offset);
 
@@ -41,7 +46,7 @@ void moveUp() {
 }
 
 void moveDown() {
-  for (pos = max_pos; pos >= min_pos; pos -= 1) { 
+  for (pos = max_pos; pos >= min_pos; pos -= servo_step) { 
     mid_left_pitch_servo.write(pos);              
     mid_left_shin_servo.write(pos+shin_offset);
 
@@ -52,7 +57,7 @@ void moveDown() {
 }
 
 void moveForward() {
-  for (pos = front_pos; pos <= back_pos; pos += 1) { 
+  for (pos = front_pos; pos <= back_pos; pos += servo_step) { 
     mid_left_hip_servo.write(pos);             
     front_left_hip_servo.write(back_pos - pos + front_hip_offset);             
     delay(delay_ms);                       
@@ -60,7 +65,7 @@ void moveForward() {
 }
 
 void moveBack() {
-  for (pos = back_pos; pos >= front_pos; pos -= 1) { 
+  for (pos = back_pos; pos >= front_pos; pos -= servo_step) { 
     mid_left_hip_servo.write(pos);             
     front_left_hip_servo.write(back_pos - pos + front_hip_offset);             
     delay(delay_ms);                       
@@ -68,8 +73,16 @@ void moveBack() {
 }
 
 void loop() {
-  moveUp();
-  moveForward();
-  moveDown();
-  moveBack();
+  centered_servo.write(90);
+  delay(1000);
+  
+  //centered_servo.write(0);
+  //delay(1000);
+
+  //centered_servo.write(180);
+  //delay(1000);
+  //moveUp();
+  //moveForward();
+  //moveDown();
+  //moveBack();
 }
