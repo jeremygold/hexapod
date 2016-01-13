249b78a8f7053f68dea2a6ed8a6aac46aeef02d6
Jeremy Gold <jeremy.gold@ikegps.com>
2016-01-13 09:07:05 +0000

    Updating to operate with full GPIO configuration

--- Rpi/hexapod.py
+++ Rpi/hexapod.py
@@ -3,33 +3,102 @@
 import time
 import os
 
-STEP = 1
-DELAY = 0.001
+STEP = 5
+DELAY = 0.005
+offset = 290
+CENTER = 90
+SWING = 45
+MIN = CENTER - SWING
+MAX = CENTER + SWING
+
+LEFT = 1
+RIGHT = 0
+
+frontLeftShin = 3
+frontLeftThigh = 4
+frontLeftHip = 5
+
+frontRightHip = 6
+frontRightThigh = 7
+frontRightShin = 8
  
 def pwm(pin, angle):
-  print "servo[" + str(pin) + "][" + str(angle) + "]"
-  cmd = "echo " + str(pin) + "=" + str(angle) + " > /dev/servoblaster"
+  # Compensate for 1000 - 2000us representing +/- 90 degrees, and ServoBlaster is in units of 10us
+  pwmDelay = 150 + ((angle - 90) * 50 / 90)
+  print "servo[" + str(pin) + "][" + str(angle) + "=" + str(pwmDelay*10) + "us]"
+  cmd = "echo " + str(pin) + "=" + str(pwmDelay) + " > /dev/servoblaster"
   os.system(cmd)
 
-def legUpDown():
-  for j in range(90, 170, STEP):
-    pwm(1,j)
-    pwm(2,j)
-    # time.sleep(DELAY)
-  for j in range(170, 90, (STEP*-1)):
-    pwm(1,j)
-    pwm(2,j)
-    # time.sleep(DELAY)
+def moveCenter(motor):
+  pwm(motor,CENTER)
 
-def legForwardBack():
-  for j in range(90, 170, STEP):
-    pwm(3,j)
-    # time.sleep(DELAY)
-  for j in range(170, 90, (STEP*-1)):
-    pwm(3,j)
-    # time.sleep(DELAY)
+def moveTest(motor, side):
+  for j in range(MIN, MAX, STEP):
+    if side == LEFT:
+      pwm(motor,j)
+    else:
+      pwm(motor,180-j)
+    time.sleep(DELAY)
+
+  for j in range(MAX, MIN, (STEP*-1)):
+    if side == LEFT:
+      pwm(motor,j)
+    else:
+      pwm(motor,180-j)
+    time.sleep(DELAY)
+
+  time.sleep(1)
+
+def legUp():
+  for j in range(MIN, MAX, STEP):
+    pwm(frontLeftShin,j)
+    pwm(frontLeftThigh,j)
+
+    pwm(frontRightShin,(MAX - j) + MIN)
+    pwm(frontRightThigh,(MAX - -j) + MIN)
+    time.sleep(DELAY)
+
+def legDown():
+  for j in range(MAX, MIN, (STEP*-1)):
+    pwm(frontLeftShin,j)
+    pwm(frontLeftThigh,j)
+
+    pwm(frontRightShin,(MAX - j) + MIN)
+    pwm(frontRightThigh,(MAX - j) + MIN)
+    time.sleep(DELAY)
+
+def legForward():
+  for j in range(MIN, MAX, STEP):
+    pwm(frontLeftHip,j)
+    pwm(frontRightHip,(MAX - j) + MIN)
+    time.sleep(DELAY)
+
+def legBack():
+  for j in range(MAX, MIN, (STEP*-1)):
+    pwm(frontLeftHip,j)
+    pwm(frontRightHip,(MAX - j) + MIN)
+    time.sleep(DELAY)
 
 while True:
-  legUpDown()
-  legForwardBack()
+  # legUp()
+  # legForward()
+  # legDown()
+  # legBack()
+  moveCenter(frontLeftShin)
+  moveCenter(frontLeftThigh)
+  moveCenter(frontLeftHip)
+
+  moveCenter(frontRightShin)
+  moveCenter(frontRightThigh)
+  moveCenter(frontRightHip)
+  time.sleep(1)
+
+  moveTest(frontLeftShin, LEFT)
+  moveTest(frontLeftThigh, LEFT)
+  moveTest(frontLeftHip, LEFT)
+
+  moveTest(frontRightShin, RIGHT)
+  moveTest(frontRightThigh, RIGHT)
+  moveTest(frontRightHip, RIGHT)
+  time.sleep(1)
 
