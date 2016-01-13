f250f59a646ffa761cb34ca1b16df1c36df38171
Jeremy Gold <jeremy.gold@ikegps.com>
2016-01-13 09:08:03 +0000

    Initial implementation of Text to speech for talking hexapods

---
+++ Rpi/HexSpeak.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+espeak -ven+f3 -k5 -s150 "Hello there Louie"
+sleep 3s
+
+espeak -ven+f3 -k5 -s150 "I bet you didn't know I could talk"
+sleep 3s
+
+espeak -ven+f3 -k5 -s150 "I hope Jeremy finishes me soon so I can walk too!"
