f92bdd75ac9c93262029b18af968bdd7df48816e
Jeremy Gold <jeremy.gold@ikegps.com>
2016-01-16 08:43:54 +0000

    Splitting out separate tests for individual, hip_ik, thigh_ik

--- Rpi/hexapod_tests.py
+++ Rpi/hexapod_tests.py
@@ -3,35 +3,40 @@
 from Leg import *
 from math import *
 import time
+from numpy import arange
 
 limit = 2.5 * pi / 8
-
 front_right = Leg(6, 7, 8, Side.right)
 
-# front_right.set_hip_angle(pi/4)
-# time.sleep(1)
-# front_right.set_hip_angle(-pi/4)
-# time.sleep(1)
-# 
-# front_right.set_thigh_angle(pi/4)
-# time.sleep(1)
-# front_right.set_thigh_angle(-pi/4)
-# time.sleep(1)
-# 
-# front_right.set_shin_angle(pi/4)
-# time.sleep(1)
-# front_right.set_shin_angle(-pi/4)
-# time.sleep(1)
+def individual_tests():
+    front_right.set_hip_angle(pi/4)
+    time.sleep(1)
+    front_right.set_hip_angle(-pi/4)
+    time.sleep(1)
 
-front_right.set_leg_pos(0, limit)
-time.sleep(1)
-front_right.set_leg_pos(0, -limit)
-time.sleep(1)
-# 
-# for i in range(0, 5):
-    # front_right.set_leg_pos(pi / 4, 0)
-    # time.sleep(1)
-    # front_right.set_leg_pos(-pi / 4, 0)
-    # time.sleep(1)
+    front_right.set_thigh_angle(pi/4)
+    time.sleep(1)
+    front_right.set_thigh_angle(-pi/4)
+    time.sleep(1)
+
+    front_right.set_shin_angle(pi/4)
+    time.sleep(1)
+    front_right.set_shin_angle(-pi/4)
+    time.sleep(1)
+
+def thigh_ik_tests():
+    front_right.set_leg_pos(0, limit)
+    time.sleep(1)
+    front_right.set_leg_pos(0, -limit)
+    time.sleep(1)
+
+def hip_ik_tests():
+    for angle in arange(-pi / 4, pi / 4, pi / 64):
+        front_right.set_leg_pos(angle, -pi / 4)
+        time.sleep(0.05)
+
+individual_tests()
+thigh_ik_tests()
+hip_ik_tests()
 
 front_right.set_leg_pos(0, 0)
