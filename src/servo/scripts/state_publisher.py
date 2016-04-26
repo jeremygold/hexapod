56172a36a4f76a8ae8d753eee9e1da16ba2e2886
Jeremy Gold <jeremy.gold@ikegps.com>
2016-04-26 21:53:55 +1200

    Updating to include web server, and monitor published state from the led_hal in RViz

--- src/servo/scripts/state_publisher.py
+++ src/servo/scripts/state_publisher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 from geometry_msgs.msg import Quaternion
@@ -9,49 +9,47 @@ from geometry_msgs.msg import TransformStamped
 import tf 
 import math
 
+joint_pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+broadcaster = tf.TransformBroadcaster()
+# message declarations
+joint_state = JointState()
+joint_state.header = Header()
+joint_state.name = ["swivel"]
+
+
+
+def updateState(data):
+    # When we receive an updated position, convert to angle, and send to tf subscriber
+    global joint_pub
+    global broadcaster
+    global joint_state
+
+    swivel = (data.data - 150) * math.pi / 180
+
+    rospy.loginfo("Updating servo position: {:d}".format(data.data))
+
+    # update joint_state
+    joint_state.header.stamp = rospy.Time.now()
+    joint_state.position = [swivel]
+    joint_state.velocity = []
+    joint_state.effort = []
+
+    # send the joint state and transform
+    joint_pub.publish(joint_state)
+
+    # update transform
+    # (moving in a circle with radius=2)
+    broadcaster.sendTransform((0, 0, 0),
+                              tf.transformations.quaternion_from_euler(0, 0, swivel + math.pi / 2),
+                              rospy.Time.now(),
+                              "axis",
+                              "odom")
+
 def move_robot():
+    global joint_pub
     rospy.init_node('state_publisher')
-    joint_pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-    broadcaster = tf.TransformBroadcaster()
-
-    degree = math.pi / 180.0
-
-    # robot state
-    swivel = 0
-    swivinc = 1 * degree
-
-    # message declarations
-    joint_state = JointState()
-    joint_state.header = Header()
-    joint_state.name = ["swivel"]
-
-    rate = rospy.Rate(30)
-
-    while not rospy.is_shutdown():
-        # update joint_state
-        joint_state.header.stamp = rospy.Time.now()
-        joint_state.position = [swivel]
-        joint_state.velocity = []
-        joint_state.effort = []
-
-        # send the joint state and transform
-        joint_pub.publish(joint_state)
-
-        # update transform
-        # (moving in a circle with radius=2)
-        broadcaster.sendTransform((0, 0, 0),
-                                  tf.transformations.quaternion_from_euler(0, 0, swivel + math.pi / 2),
-                                  rospy.Time.now(),
-                                  "axis",
-                                  "odom")
-
-        #  Create new robot state
-        swivel += swivinc
-        if swivel > math.pi / 4 or swivel < 0:
-            swivinc *= -1
-
-        #  This will adjust as needed per iteration
-        rate.sleep()
+    rospy.Subscriber('state', Int16, updateState)
+    rospy.spin()
 
 if __name__ == '__main__':
     move_robot()
