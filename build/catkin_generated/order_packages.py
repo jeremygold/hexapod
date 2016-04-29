b8da7fb2cd3d229f19c06e72089ff03c5699e4ab
Jeremy Gold <jeremy.gold@ikegps.com>
2016-04-29 21:50:01 +0000

    Tidying up package dependencies

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/pi/PubSubToRpi/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/pi/PubSubToRpi/devel;/opt/ros/indigo".split(';') if "/home/pi/PubSubToRpi/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,8 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/PubSubToRpi/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/pi/PubSubToRpi/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/pi/PubSubToRpi/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/pi/PubSubToRpi/build"
+export ROS_PACKAGE_PATH="/home/pi/PubSubToRpi/src:$ROS_PACKAGE_PATH"
