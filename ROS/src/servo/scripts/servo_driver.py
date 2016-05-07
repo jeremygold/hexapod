5633998675810d28513645b218f992ea4a814a82
Jeremy Gold <jeremy.gold@ikegps.com>
2016-05-07 10:29:13 +0000

    Adding remaining servos

--- ROS/src/servo/scripts/servo_driver.py
+++ ROS/src/servo/scripts/servo_driver.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python
-## RPI SERVO PWM HAL
-## Broadcast Servo Position (angle in degrees) at specified rate, and listen for commands to change position
+## Hexapod Servo Driver
+
+## Listen for /joint_states updates, and configure servos accordingly
+
 ## Some good reference data on HX12K servos here:
     ## http://www.rcuniverse.com/forum/rc-radios-transmitters-receivers-servos-gyros-157/11523159-hextronik-hx12k-standard-metal-gear-servo-55g-10kg-16sec.html
 
 import rospy
-import os
 
-from std_msgs.msg import Int16
 from sensor_msgs.msg import JointState
 from Servo import *
 
-rbh_servo = Servo(9)
-rbt_servo = Servo(10)
-rbs_servo = Servo(11)
+servos = dict({})
 
-rmh_servo = Servo(12)
-rmt_servo = Servo(13)
-rms_servo = Servo(14)
-
-rfh_servo = Servo(15)
-rft_servo = Servo(16)
-rfs_servo = Servo(17)
-
-lbh_servo = Servo(0)
-lbt_servo = Servo(1)
-lbs_servo = Servo(2)
-
-lmh_servo = Servo(3)
-lmt_servo = Servo(4)
-lms_servo = Servo(5)
-
-lfh_servo = Servo(6)
-lft_servo = Servo(7)
-lfs_servo = Servo(8)
-
-# Set servo position in degrees
 def set_joint_position(joint_state):
-    # rospy.loginfo("In set_joint_position, joint_state = %s", repr(joint_state))
-
     for index, name in enumerate(joint_state.name):
-        position = joint_state.position[index]
+        if name in servos:
+            position = joint_state.position[index]
+            servos[name].set_servo_angle(position)
 
-        if name == "/left/front/hip":
-            lfh_servo.set_servo_angle(position)
-        elif name == "/left/front/thigh":
-            lft_servo.set_servo_angle(position)
-        elif name == "/left/front/shin":
-            lfs_servo.set_servo_angle(position)
+def init_servos():
+    global servos
+    servos['/left/back/hip'] = Servo(0)
+    servos['/left/back/thigh'] = Servo(1)
+    servos['/left/back/shin'] = Servo(2)
+
+    servos['/left/mid/hip'] = Servo(3)
+    servos['/left/mid/thigh'] = Servo(4)
+    servos['/left/mid/shin'] = Servo(5)
+
+    servos['/left/front/hip'] = Servo(6)
+    servos['/left/front/thigh'] = Servo(7)
+    servos['/left/front/shin'] = Servo(8)
+
+    servos['/right/back/hip'] = Servo(9)
+    servos['/right/back/thigh'] = Servo(10)
+    servos['/right/back/shin'] = Servo(11)
+
+    servos['/right/mid/hip'] = Servo(12)
+    servos['/right/mid/thigh'] = Servo(13)
+    servos['/right/mid/shin'] = Servo(14)
+
+    servos['/right/front/hip'] = Servo(12)
+    servos['/right/front/thigh'] = Servo(13)
+    servos['/right/front/shin'] = Servo(14)
 
 def monitor_servos():
     rospy.Subscriber('/joint_states', JointState, set_joint_position)
@@ -56,7 +52,7 @@ def monitor_servos():
 if __name__ == '__main__':
     try:
         rospy.init_node('servo_hal', anonymous=False)
-        rospy.loginfo("Initializing")
+        init_servos()
         monitor_servos()
 
     except rospy.ROSInterruptException:
