7d1a8c9a847112d26d3969a902760b9d31eb582a
Jeremy Gold <jeremy.gold@ikegps.com>
2016-05-11 09:59:27 +0000

    Initial creation of HexapodApp with gait_command subscriber

---
+++ ROS/src/hexapod_app/scripts/HexapodApp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from time import sleep
+from Robot import Robot
+from IdleGait import IdleGait
+from WalkGait import WalkGait
+from PressupGait import PressupGait
+
+class HexapodApp:
+    def __init__(self):
+        rospy.init_node('hexapod_app', anonymous=True)
+        self.robot = Robot()
+        rospy.Subscriber('gait_command', String, self.handle_gait_command)
+
+    def handle_gait_command(self, data):
+        command = data.data
+        rospy.loginfo("Changing Gait to " + command)
+
+        if command == "Idle":
+            self.robot.gait = IdleGait()
+
+        elif command == "Walk":
+            self.robot.gait = WalkGait()
+
+        elif command == "Pressup":
+            self.robot.gait = PressupGait()
+
+        else:
+            self.robot.gait = IdleGait()
+
+
+    def run(self):
+        r = rospy.Rate(50)
+
+        while not rospy.is_shutdown():
+            self.robot.update()
+            r.sleep()
+
+if __name__ == '__main__':
+    app = HexapodApp()
+    app.run()
+
