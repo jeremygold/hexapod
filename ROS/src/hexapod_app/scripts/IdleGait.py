1b3ffda7317d8dc1480e3ff97a8ffd412504930a
Jeremy Gold <jeremy.gold@ikegps.com>
2016-05-12 09:49:41 +0000

    Updating Idle to reset to zero position

--- ROS/src/hexapod_app/scripts/IdleGait.py
+++ ROS/src/hexapod_app/scripts/IdleGait.py
@@ -3,5 +3,9 @@ from Gait import Gait
 
 class IdleGait(Gait):
     def update(self, robot):
-        # rospy.loginfo("IdleGait update")
-        pass
+        robot.left_front.set_leg_pos(0, 0)
+        robot.left_mid.set_leg_pos(0, 0)
+        robot.left_back.set_leg_pos(0, 0)
+        robot.right_front.set_leg_pos(0, 0)
+        robot.right_mid.set_leg_pos(0, 0)
+        robot.right_back.set_leg_pos(0, 0)
