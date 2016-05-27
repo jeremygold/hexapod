971ff92c8a58731127bb9f2bb8c7f268fdc36944
Jeremy Gold <jeremy.gold@ikegps.com>
2016-05-27 18:49:35 +1200

    Updating to run correct setup script depending on OS

--- ROS/initRos.sh
+++ ROS/initRos.sh
@@ -7,11 +7,13 @@ export ROS_MASTER_URI=http://HexapodPi:11311
 # export ROS_MASTER_URI=http://ubuntu:11311
 
 # Note: ROS_IP is this hosts IP address
-if [ "$HOSTNAME" = ubuntu ]; then
+if [ "$HOSTNAME" = ubuntu ]
+then
     export ROS_IP=192.168.1.123
+    source /pub/projects/hexapod/ROS/devel/setup.zsh
 else
     export ROS_IP=192.168.1.128
+    source /home/pi/hexapod/ROS/devel/setup.zsh
 fi
 
-source /home/pi/hexapod/ROS/devel/setup.zsh
 
