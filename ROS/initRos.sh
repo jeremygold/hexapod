fb3ecf88ce16817be8f121bbf05ea3a689fc4864
Jeremy Gold <jeremy.gold@ikegps.com>
2016-07-31 08:25:59 +0000

    Updating to use new IP address

--- ROS/initRos.sh
+++ ROS/initRos.sh
@@ -7,12 +7,12 @@ export ROS_MASTER_URI=http://HexapodPi:11311
 # export ROS_MASTER_URI=http://ubuntu:11311
 
 # Note: ROS_IP is this hosts IP address
-if [ "$HOSTNAME" = ubuntu ]
+if [ `hostname` = Cappucino ]
 then
-    export ROS_IP=192.168.1.123
+    export ROS_IP=192.168.1.107
     source /pub/projects/hexapod/ROS/devel/setup.zsh
 else
-    export ROS_IP=192.168.1.128
+    export ROS_IP=192.168.1.117
     source /home/pi/hexapod/ROS/devel/setup.zsh
 fi
 
