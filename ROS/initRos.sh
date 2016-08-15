d451670de15b70ca876dc69b46153bfa8c4e04fc
Jeremy Gold <jeremy.gold@ikegps.com>
2016-08-15 20:13:45 +1200

    Updating initRos.sh to be more generic

--- ROS/initRos.sh
+++ ROS/initRos.sh
@@ -3,18 +3,24 @@
 # Master is running on HexapodPi
 # export ROS_MASTER_URI=http://HexapodPi:11311
 #
-# Master is running on ubuntu VM
-# export ROS_MASTER_URI=http://ubuntu:11311
+# Master is running on current host
+export ROS_MASTER_URI=http://`hostname`:11311
 
 # Note: ROS_IP is this hosts IP address
-if [ `hostname` = Cappuccino ]
-then
-    export ROS_IP=192.168.1.107
-    export ROS_MASTER_URI=http://Cappuccino:11311
-    source /pub/projects/hexapod/ROS/devel/setup.zsh
-else
-    export ROS_IP=192.168.1.117
-    source /home/pi/hexapod/ROS/devel/setup.zsh
-fi
+case `hostname` in 
+    ubuntu-vbox)
+        export ROS_IP=10.0.2.15
+        source /pub/projects/hexapod/ROS/devel/setup.zsh
+        ;;
+
+    Cappuccino)
+        export ROS_IP=192.168.1.107
+        source /pub/projects/hexapod/ROS/devel/setup.zsh
+        ;;
+    *)
+        export ROS_IP=192.168.1.117
+        source /home/pi/hexapod/ROS/devel/setup.zsh
+        ;;
+esac
 
 
