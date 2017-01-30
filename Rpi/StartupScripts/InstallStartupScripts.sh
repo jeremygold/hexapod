64278f5ddba411dffcbe0574899a91d4e6e1b6e2
Jeremy Gold <jeremy.gold@ikegps.com>
2017-01-30 09:18:55 +0000

    Updating startup scripts to include Hexapod ROS startup

--- Rpi/StartupScripts/InstallStartupScripts.sh
+++ Rpi/StartupScripts/InstallStartupScripts.sh
@@ -10,5 +10,12 @@ cp servoblaster /etc/init.d/
 chmod 755 /etc/init.d/servoblaster
 /etc/init.d/servoblaster start
 
+# Install and run hexapod_ros init script
+cp hexapod_ros /etc/init.d/
+chmod 755 /etc/init.d/hexapod_ros
+/etc/init.d/hexapod_ros start
+
 # Update to run by default
 update-rc.d servoblaster defaults
+update-rc.d hexapod_ros defaults
+
